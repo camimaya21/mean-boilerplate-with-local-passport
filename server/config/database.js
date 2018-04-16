e276238e6487a08a36f92528a37a6671a862cac1
Camila <camilamaya21@gmail.com>
2018-04-16 13:04:10 +0200

    test bd connection

--- server/config/database.js
+++ server/config/database.js
@@ -3,14 +3,14 @@ const path = require('path');
 const debug = require('debug')("server:"+path.basename(__filename).split('.')[0]);
 const mongoose = require('mongoose');
 //if you're working environment variables
-//const dbURL = process.env.DBURL
+const DBURL = process.env.DBURL
 //if you're running a local DB without environment variables
-const dbURL = 'mongodb://localhost:27017/myproject';
+//const DBURL = 'mongodb://localhost:27017/myproject';
 
-  mongoose.connect(dbURL,{useMongoClient:true})
+  mongoose.connect(DBURL,{useMongoClient:true})
   .then(() =>{
-    debug(`connected to database ${dbURL}`)
+    debug(`connected to database ${DBURL}`)
   }).catch((err) => {
-    debug(`ERROR CONNECTING TO DB ${dbURL}`);
+    debug(`ERROR CONNECTING TO DB ${DBURL}`);
     throw err;
   })
